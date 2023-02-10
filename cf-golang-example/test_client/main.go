73160adfdfc65d196e7e90c9c46212a2ce83741d
Tao Deng <devtdeng@users.noreply.github.com>
2023-02-10 12:59:27 +0100

    Read threads count from environment variable

--- cf-golang-example/test_client/main.go
+++ cf-golang-example/test_client/main.go
@@ -12,18 +12,16 @@ import (
 // refer to test code at https://github.com/golang/go/issues/34902
 // https://gist.github.com/KauzClay/2d84e0ce1d1884a7bf3ca952ebb57630
 func main() {
-	var n int
 	// 10 goroutines is a good number to see the panic happen quickly on
 	// our test environments, though this may not apply to all machines
-	flag.IntVar(&n, "n", 1, "number of goroutines")
-	flag.Parse()
+	threads := os.Getenv("THRAED_NUM", 10)
 
 	// Request needs a larger body to see the error happen more quickly
 	// It is reproducable with smaller body sizes, but it takes longer to fail
 	bodyString := strings.Repeat("a", 2048)
 	client := &http.Client{}
 
-	for i := 0; i < n; i++ {
+	for i := 0; i < threads; i++ {
 		go func() {
 			for {
 				buf := bytes.NewBufferString(bodyString)
